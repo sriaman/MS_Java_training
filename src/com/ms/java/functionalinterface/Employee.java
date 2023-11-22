d420cd0889077a08b549e2ee9ae5ed607c7f4450
sriaman <amansrivastav.cse20@jecrc.ac.in>
2023-11-22 23:39:35 +0530

    Add Stream Api Assignments

--- src/com/ms/java/functionalinterface/Employee.java
+++ src/com/ms/java/functionalinterface/Employee.java
@@ -1,7 +1,8 @@
 package com.ms.java.functionalinterface;
 
 import java.time.LocalDate;
-import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Employee {
 
@@ -12,6 +13,8 @@ public class Employee {
     private int salary;
     private String dept;
 
+    private LocalDate joiningDate;
+
 
     public Employee(String firstName, String lastName, int id, LocalDate dateOfBirth, int salary, String dept) {
         this.firstName = firstName;
@@ -20,6 +23,24 @@ public class Employee {
         this.dateOfBirth = dateOfBirth;
         this.salary = salary;
         this.dept = dept;
+    }
+
+    public Employee(String firstName, String lastName, int id, LocalDate dateOfBirth, int salary, String dept, LocalDate joiningDate) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.id = id;
+        this.dateOfBirth = dateOfBirth;
+        this.salary = salary;
+        this.dept = dept;
+        this.joiningDate = joiningDate;
+    }
+
+    public LocalDate getJoiningDate() {
+        return joiningDate;
+    }
+
+    public void setJoiningDate(LocalDate joiningDate) {
+        this.joiningDate = joiningDate;
     }
 
     public String getFirstName() {
@@ -80,5 +101,17 @@ public class Employee {
                 ", salary=" + salary +
                 ", dept='" + dept + '\'' +
                 '}';
+    }
+
+    public static List<Employee> getAllEmployee(){
+        List<Employee> empList = new ArrayList<>();
+        empList.add(new Employee("Aman", "Srivastav",1001, LocalDate.of(2000,11,22),123456,"IT",LocalDate.of(2023,01,02)));
+        empList.add(new Employee("Saket", "Srivastav",1002,LocalDate.of(1997,12,21),1236,"HR",LocalDate.of(2023,02,25)));
+        empList.add(new Employee("Harsh", "Srivastav",1003,LocalDate.of(1998,01,20),1265,"HR",LocalDate.of(2022,11,28)));
+        empList.add(new Employee("Amrita", "Srivastav",1004,LocalDate.of(1992,11,22),2313,"Finance",LocalDate.of(2020,11,02)));
+        empList.add(new Employee("Vivek", "Srivastav",1005,LocalDate.of(1995,11,22),123987,"HR",LocalDate.of(2023,05,22)));
+        empList.add(new Employee("Siddharth", "Kumar",1006,LocalDate.of(2003,11,22),123675,"Finance",LocalDate.of(2021,10,26)));
+
+        return empList;
     }
 }
---
+++ src/com/ms/java/streams/StreamDemo.java
@@ -0,0 +1,51 @@
+package com.ms.java.streams;
+
+import com.ms.java.functionalinterface.Employee;
+
+import java.util.*;
+import java.util.function.BinaryOperator;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class StreamDemo {
+    public static void main(String[] args) {
+
+        //1. Write a program to print First Name of employees which joined in year 2023.
+        List<Employee> empList = Employee.getAllEmployee();
+        empList.stream().filter(x->x.getJoiningDate().getYear()==2023).forEach(x->System.out.println(x.getFirstName()));
+
+        //2. Write a program to print count, min, max, sum, and the average of salary of all employees in a particular department.
+        //count
+        Map<String,Long> map = empList.stream().collect(Collectors.groupingBy(x->x.getDept(),Collectors.counting()));
+        map.entrySet().stream().forEach(x-> System.out.println(x.getKey()+" "+x.getValue()));
+
+        //min
+        Map<String, Optional<Employee>> map2 = empList.stream().collect(Collectors.groupingBy(Employee::getDept, Collectors.minBy(Comparator.comparing(Employee::getSalary))));
+        map2.entrySet().stream().forEach(x-> System.out.println(x.getKey()+" "+x.getValue().get().getSalary()));
+
+        //max
+        Map<String, Optional<Employee>> map3 = empList.stream().collect(Collectors.groupingBy(x->x.getDept(),Collectors.maxBy(Comparator.comparing(Employee::getSalary))));
+        map3.entrySet().forEach(x-> System.out.println(x.getKey()+" "+x.getValue().get().getSalary()));
+
+        //sum
+        Map<String, IntSummaryStatistics> map4 = empList.stream().collect(Collectors.groupingBy(Employee::getDept,Collectors.summarizingInt(x->x.getSalary())));
+        map4.entrySet().stream().forEach(x-> System.out.println(x.getKey()+" "+x.getValue().getSum()));
+
+        //average
+        map4.entrySet().stream().forEach(x-> System.out.println(x.getKey()+" "+x.getValue().getAverage()));
+
+        //3. Write a program to print sorted list of employees by firstName in all departments except HR department.
+        empList.stream().filter(x->x.getDept()!="HR").sorted(Comparator.comparing(x->x.getFirstName())).forEach(System.out::println);
+
+        //4. Write a program to increment salary of employees in particular department by 10%.
+        empList.stream().map(x->x.getSalary()*1.1).forEach(System.out::println);
+//        Map<String,List<Employee>> map5 = empList.stream().map(x->x.getSalary()*1.1).collect(Collectors.groupingBy(Employee::getDept));
+
+        //5. Write a program using stream to print 50 odd numbers after 100.
+        IntStream.range(100,200).filter(x->x%2==1).forEach(System.out::println);
+
+        //6. Write a program to create comma seperated list of First names of employees order by dateOfbirth.
+        String str = empList.stream().sorted(Comparator.comparing(x->x.getDateOfBirth())).map(x->x.getFirstName()).collect(Collectors.joining(", "));
+        System.out.println(str);
+    }
+}
